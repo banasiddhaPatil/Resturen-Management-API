ea44f5c3d819435394cbb2038031851a6ab4dc82
Hrishi456-cmd <rkhandbhor45@gmail.com>
2023-07-22 22:37:13 +0530

    submitted

---
+++ src/main/java/com/geekster/HotelManagements/HotelManagementsApplication.java
@@ -0,0 +1,13 @@
+package com.geekster.HotelManagements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekster/HotelManagements/HotelManagementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.HotelManagements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
